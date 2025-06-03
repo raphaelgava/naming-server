2b0656e83b362a598415e8fd65612d41d482a048
Raphael Gava <raphaelgava1@gmail.com>
2025-06-03 18:38:43 -0300

    docker-compose.yml added

--- src/main/java/com/naming_server/NamingServerApplication.java
+++ src/main/java/com/naming_server/NamingServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaServer //funciona como um registro de serviços em arquiteturas de microserviços. Isso permite que diferentes serviços se registrem e descubram uns aos outros dinamicamente.
 public class NamingServerApplication {
 
 	public static void main(String[] args) {
